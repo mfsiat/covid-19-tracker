30e4a759799d646f3bd04f90aec37b19e9d0bd7c
Nasirul Islam <nasirul.siat@outlook.com>
2020-04-14 21:01:13 +0600

    deleted original src and created from scratch

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// class based component
+import React from "react";
+
+class app extends Rreact.Component {
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+      </div>
+    );
+  }
+}
+
+export default app;
