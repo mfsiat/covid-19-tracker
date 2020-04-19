bb24231a8e9e5323f9f120a1243464b261d712f9
Nasirul Islam <nasirul.siat@outlook.com>
2020-04-20 03:05:55 +0600

    done with fetching data

--- src/app.js
+++ src/app.js
@@ -1,16 +1,32 @@
 // class based component
 import React from "react";
 
-import Cards from './component/Cards/Cards';
-import Chart from './component/Chart/Chart';
-import CountryPicker from './component/CountryPicker/CountryPicker';
-
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
 
 class app extends React.Component {
+  // setting a state to display the data on the cards component
+  state = {
+    data: {},
+  }
+
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+
   render() {
+    // destructuring the this.data 
+    const { data } = this.state;
+
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards data={data}/>
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
-const Cards = () => {
-  return <h1>Cards</h1>;
+import styles from './Cards.module.css';
+
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  // console.log(props);
+  if(!confirmed){
+    return 'loading.......'
+  }
+  return (
+  <div className={styles.container}>
+    <Grid container spacing={3} justify="center">
+      <Grid item component={Card}>
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>Infected</Typography>
+          <Typography variant="h5">{confirmed.value}</Typography>
+          <Typography color="textSecondary">Real Date</Typography>
+          <Typography variant="body2">Number of Active cases of COVID-19</Typography>
+        </CardContent>
+      </Grid>
+      <Grid item component={Card}>
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+          <Typography variant="h5">{recovered.value}</Typography>
+          <Typography color="textSecondary">Real Date</Typography>
+          <Typography variant="body2">Number of Recovered cases of COVID-19</Typography>
+        </CardContent>
+      </Grid>
+      <Grid item component={Card}>
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+          <Typography variant="h5">{deaths.value}</Typography>
+          <Typography color="textSecondary">Real Date</Typography>
+          <Typography variant="body2">Number of Deaths caused by COVID-19</Typography>
+        </CardContent>
+      </Grid>
+
+    </Grid>
+  </div>
+  )
 };
 
 export default Cards;
