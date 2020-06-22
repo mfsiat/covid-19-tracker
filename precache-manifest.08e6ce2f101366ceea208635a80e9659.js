f4a5f44fbe50087b12d7dbd573d1a1d296f44ff3
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2020-06-22 21:23:58 +0600

    Updates

---
+++ precache-manifest.08e6ce2f101366ceea208635a80e9659.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435d7d348764f9e96e526c814244ed36",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "2021d11414801b9058a1",
+    "url": "/covid-19-tracker/static/css/main.9633d242.chunk.css"
+  },
+  {
+    "revision": "ed3ed82f15d776933596",
+    "url": "/covid-19-tracker/static/js/2.9b10ad4e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.9b10ad4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2021d11414801b9058a1",
+    "url": "/covid-19-tracker/static/js/main.90897dd5.chunk.js"
+  },
+  {
+    "revision": "131a3f8a707da252ef4a",
+    "url": "/covid-19-tracker/static/js/runtime-main.3c8d6362.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
